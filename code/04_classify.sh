d109406648b81a21cd6a32274bfca43e1369e301
rls1076 <rls1076@usnh.edu>
2026-04-24 16:26:02 -0400

    updates

--- code/04_classify.sh
+++ code/04_classify.sh
@@ -1,21 +1,21 @@
 #!/bin/bash
 
-primer="16s_V4-V5"
-projname="YOURPROJ_${primer}"
+primer="18s"
+projname="Estuaries_${primer}"
 
 ## Classifiy
-refreads=${refdbs/16s/ref_seqs_16S_V4-V5.qza}
-reftax=${refdbs/16s/99_otus_16S_taxonomy.qza}
-sklearn=${refdbs/16s/silva_99_otus_16S_nb-classifier.qza}
+reftax=${reftax:-/home/users/jtm1171/refdbs/18s/SILVA/silva-138-99-tax.qza}
+refreads=${refreads:-/home/users/jtm1171/refdbs/18s/SILVA/silva-138-99-seqs-pid_0.65-extracted.qza}
+sklearn=${sklearn:-/home/users/jtm1171/refdbs/18s/SILVA/silva-138-99-seqs-pid_0.65-classifier.qza}
 
 ## copied from qiime2_parameters.sh
 maxaccepts=10
-query_cov=0.75 
-perc_identity=0.75 
-weak_id=0.65
+query_cov=0.8 
+perc_identity=0.90 
+weak_id=0.80
 
 qiime feature-classifier classify-hybrid-vsearch-sklearn \
-  --i-query results/${projname}_rep-seqs.qza \
+  --i-query data/results/${projname}_rep-seqs.qza \
   --i-classifier ${sklearn} \
   --i-reference-reads ${refreads} \
   --i-reference-taxonomy  ${reftax} \
@@ -27,4 +27,4 @@ qiime feature-classifier classify-hybrid-vsearch-sklearn \
   --p-maxhits all \
   --p-min-consensus 0.51 \
   --p-confidence 0.7 \
-  --o-classification results/${projname}_hybrid_taxonomy
+  --o-classification data/results/${projname}_hybrid_taxonomy
