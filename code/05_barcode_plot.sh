555f33e5379a604eb560e8d081eb23146d314ad3
rls1076 <rls1076@usnh.edu>
2026-05-13 23:01:25 -0400

    chnages

--- code/05_barcode_plot.sh
+++ code/05_barcode_plot.sh
@@ -3,10 +3,10 @@
 ## replace with the qiime2 environment youve been using 
 screen
 
+conda activate qiime2-amplicon-2026.1
+
 primer="18s"
 projname="Estuaries_${primer}"
-
-conda activate qiime2-amplicon-2026.1
 
 ### This script will create a barcode plot of the taxonomic composition of each sample. It will use the qiime2 feature-table and taxonomy files to create a stacked bar plot of the relative abundance of each taxon in each sample. The plot will be saved as a .png file in the data/results directory.
 ### NOTE: Qiime view appears to be working only in chrome. 
@@ -38,7 +38,7 @@ qiime phylogeny align-to-tree-mafft-fasttree \
    --o-alignment data/results/${projname}_aligned-rep-seqs \
    --o-masked-alignment data/results/${projname}_masked-aligned-rep-seqs.qza\
    --o-tree data/results/${projname}_unrooted-tree.qza\
-   --o-rooted-tree data/results/${projname}_rooted-tree.qza\
+   --o-rooted-tree plots/${projname}_rooted-tree.qza\
    --p-n-threads 24
 
 ### Core Metrics (this will generate the alpha and beta diversity metrics for each sample, which will be used in the next script to create a PCoA plot of the beta diversity metrics)
